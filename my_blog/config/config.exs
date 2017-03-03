bae66c620a45fc2d32841b0fa76d35b33f9406d9
Iskandar Rizki <iskandar.rizki@detik.com>
2017-03-03 17:07:57 +0700

    add quantum for cron function

--- my_blog/config/config.exs
+++ my_blog/config/config.exs
@@ -22,6 +22,12 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :quantum, :my_blog,
+  cron: [
+    # Every minute
+    "* * * * *":      {"MyBlog.Counter", :save_all},
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
